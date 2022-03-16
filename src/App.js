d64303079fc277890b7f273f476c40102059abbb
Pablo Romero Abadie <pabloabadie1995@gmail.com>
2022-03-16 15:55:41 -0300

    se realizo el crear o alta de productos

--- src/App.js
+++ src/App.js
@@ -38,8 +38,8 @@ function App() {
       <main>
       <Routes>
         <Route exact path="/" element={<Home />}/>
-        <Route exact path="/product/table" element={<ProductsTable products={products} />}/>
-        <Route exact path="/product/create" element={<ProductCreate/>}/>
+        <Route exact path="/product/table" element={<ProductsTable  products={products} />}/>
+        <Route exact path="/product/create" element={<ProductCreate  URL={URL} getApi={getApi}/>}/>
         <Route exact path="/product/edit" element={<ProductEdit/>}/>
         <Route exact path="*" element={<Error404/>}/>
       </Routes>
